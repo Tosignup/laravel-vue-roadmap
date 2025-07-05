f6abaf8713868ba5bdc98f615b7804962688be29
Tosignup <johnmanaloto73@gmail.com>
2025-07-05 22:47:28 +0800

    Day 10 - Done

--- notes/day10.md
+++ notes/day10.md
@@ -1,13 +1,17 @@
 # Day 10 Note
 
 ## Topics Covered
-- 
+
+- Consuming Laravel API with Axios + Types
 
 ## Commands Used
-- 
+
+- See Vue 3 Consuming Laravel API in [day10/README.md](../day10/README.md)
 
 ## Things I Learned
-- 
+
+- See [day10/README.md](../day10/README.md)
 
 ## Challenges I Faced
-- 
+
+- Nothing....
---
+++ vue-frontend/src/components/AxiosTesting.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useAxios } from '@/composables/useAxios';
+
+const { posts, loading} = useAxios();
+</script>
+<template>
+  <p v-if="loading">loading...</p>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <span>{{ post.title }}</span>
+      {{ post.body }}
+    </li>
+  </ul>
+
+</template>
